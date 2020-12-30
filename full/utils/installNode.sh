8451f5b58d2677122b3f84c784626afa259b789d
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-12-30 16:37:00 +0100

    Node

--- full/utils/installNode.sh
+++ full/utils/installNode.sh
@@ -1,3 +1,3 @@
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 #RUN ln -s /usr/bin/nodejs /usr/bin/node
