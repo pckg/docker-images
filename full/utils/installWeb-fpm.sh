85417db032f0b05fc77eb0cd807ac2fd1a643dd6
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-01-16 15:52:18 +0100

    Update php

--- full/utils/installWeb-fpm.sh
+++ full/utils/installWeb-fpm.sh
@@ -1,4 +1,4 @@
-add-apt-repository ppa:ondrej/php \
+add-apt-repository ppa:ondrej/php
 add-apt-repository ppa:ondrej/apache2
 apt-get update
 apt-get install -y apache2 libapache2-mod-fcgid php7.4 php7.4-fpm php7.4-curl php7.4-intl php7.4-xml php7.4-mbstring php7.4-zip php7.4-mysql php7.4-cli php-amqplib php-ssh2 php7.4-zmq php7.4-imap php7.4-redis php7.4-bcmath php7.4-gd php7.4-bz2 php7.4-soap wget xvfb git zip unzip mysql-client
