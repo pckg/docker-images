618b643e7237835a0f41429a650d18972d65a174
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-03-15 19:34:35 +0100

    Chromium

--- full/utils/installPuppeteer.sh
+++ full/utils/installPuppeteer.sh
@@ -1,3 +1,6 @@
-#apt-get install chromium
 apt-get -y update
-apt-get install -y chrome libgbm-dev libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libasound2 libatk1.0-0 libgtk-3-0
+apt-get install -y libgbm-dev libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libasound2 libatk1.0-0 libgtk-3-0
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
