90324f355911342d713aeb7ab52fc98aaf88d93e
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-03-26 20:51:42 +0100

    Migrate snappy to php8

---
+++ full/utils/installWeb-fpm8.sh
@@ -0,0 +1,5 @@
+add-apt-repository ppa:ondrej/php
+add-apt-repository ppa:ondrej/apache2
+apt-get -y update
+apt-get install -y mysql-client postgresql-client apache2 libapache2-mod-fcgid php php-fpm php-curl php-intl php-xml php-mbstring php-zip php-mysql php-pgsql php-cli php-amqplib php-ssh2 php-zmq php-imap php-redis php-bcmath php-gd php-bz2 php-soap php-dev wget xvfb git zip unzip
+#php-pcntl
