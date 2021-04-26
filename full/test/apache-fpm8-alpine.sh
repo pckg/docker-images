9477da48b94e1284b7f8dd63136fe930af37426f
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-04-26 13:32:17 +0200

    Downgrade node

--- full/test/apache-fpm8-alpine.sh
+++ full/test/apache-fpm8-alpine.sh
@@ -19,7 +19,7 @@ matchCommandWithOutput "php -v" "PHP 8.0." "Missing or invalid PHP version"
 [ -f /usr/bin/phpize ] || (echo "No phpize or missing php-dev" && exit 1)
 
 matchCommandWithOutput "node --version" "v14.16." "Missing or invalid node version"
-matchCommandWithOutput "npm --version", "7.7." "Missing or invalid npm version"
+matchCommandWithOutput "npm --version", "6.14." "Missing or invalid npm version"
 matchCommandWithOutput "yarn --version" "1.22." "Missing or invalid yarn version"
 matchCommandWithOutput "composer --version" "Composer version 2." "Missing or invalid composer"
 
