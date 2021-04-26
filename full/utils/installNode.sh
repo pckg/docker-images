850104029327cb3f1a0b174eb238f06f66e484f0
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-04-26 13:26:29 +0200

    Update ubuntu

--- full/utils/installNode.sh
+++ full/utils/installNode.sh
@@ -1,5 +1,5 @@
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get -y update
 apt-get install -y nodejs
-npm install -g npm
+npm install -g npm@6
 #RUN ln -s /usr/bin/nodejs /usr/bin/node
