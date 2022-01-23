10d2c602c1221a062936f12ab3b18f82600f0a5d
Bojan Rajh <schtr4jh@schtr4jh.net>
2022-01-23 21:18:31 +0100

    amqplib

--- full/utils/installWeb-fpm-80.sh
+++ full/utils/installWeb-fpm-80.sh
@@ -1,5 +1,5 @@
 add-apt-repository ppa:ondrej/php
 add-apt-repository ppa:ondrej/apache2
 apt-get -y update
-apt-get install -y mysql-client postgresql-client apache2 libapache2-mod-fcgid php8.0 php8.0-fpm php8.0-curl php8.0-intl php8.0-xml php8.0-mbstring php8.0-zip php8.0-mysql php8.0-pgsql php8.0-cli php-amqplib php8.0-ssh2 php8.0-zmq php8.0-imap php8.0-redis php8.0-bcmath php8.0-gd php8.0-bz2 php8.0-soap php8.0-dev wget xvfb git zip unzip
+apt-get install -y mysql-client postgresql-client apache2 libapache2-mod-fcgid php8.0 php8.0-fpm php8.0-curl php8.0-intl php8.0-xml php8.0-mbstring php8.0-zip php8.0-mysql php8.0-pgsql php8.0-cli php8.0-amqplib php8.0-ssh2 php8.0-zmq php8.0-imap php8.0-redis php8.0-bcmath php8.0-gd php8.0-bz2 php8.0-soap php8.0-dev wget xvfb git zip unzip
 #php-pcntl
